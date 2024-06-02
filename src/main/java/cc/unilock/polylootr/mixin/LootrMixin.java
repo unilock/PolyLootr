10bd37ba31be664c773f6b2b9d58d603c7d95195
unilock <unilock@fennet.rentals>
2024-06-02 15:16:49 -0400

    fix build
    Signed-off-by: unilock <unilock@fennet.rentals>

--- src/main/java/cc/unilock/polylootr/mixin/LootrMixin.java
+++ src/main/java/cc/unilock/polylootr/mixin/LootrMixin.java
@@ -16,7 +16,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Lootr.class)
 public class LootrMixin {
-	@Inject(method = "onInitialize", at = @At("TAIL"))
+	@Inject(method = "onInitialize", at = @At("TAIL"), remap = false)
 	private void onInitialize(CallbackInfo ci) {
 		PolymerItemUtils.ITEM_CHECK.register((itemStack) ->
 			itemStack.is(ModItems.BARREL)
