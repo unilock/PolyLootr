08bd607a05c816fceeeb80c8ef9916c2d35177a1
unilock <unilock@fennet.rentals>
2024-07-28 14:10:45 -0400

    properly send vanilla BlockEntityTypes for PolymerBlock implementers
    Signed-off-by: unilock <unilock@fennet.rentals>

--- src/main/java/cc/unilock/polylootr/mixin/blocks/LootrBarrelBlockMixin.java
+++ src/main/java/cc/unilock/polylootr/mixin/blocks/LootrBarrelBlockMixin.java
@@ -1,7 +1,11 @@
 package cc.unilock.polylootr.mixin.blocks;
 
 import eu.pb4.polymer.core.api.block.PolymerBlock;
+import eu.pb4.polymer.core.api.block.PolymerBlockUtils;
+import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 import noobanidus.mods.lootr.block.LootrBarrelBlock;
 import org.spongepowered.asm.mixin.Mixin;
@@ -11,5 +15,10 @@ public class LootrBarrelBlockMixin implements PolymerBlock {
 	@Override
 	public BlockState getPolymerBlockState(BlockState state) {
 		return Blocks.BARREL.withPropertiesOf(state);
+	}
+
+	@Override
+	public void onPolymerBlockSend(BlockState blockState, BlockPos.MutableBlockPos pos, ServerPlayer player) {
+		player.connection.send(PolymerBlockUtils.createBlockEntityPacket(pos, BlockEntityType.BARREL, null));
 	}
 }
